07f4f8614949caf842828ddd1bcb8a0d9d74e602
faouzi0007 <faouziseddouki49@gmail.com>
2022-08-12 16:42:58 +0100

    New Comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Faouzi0007" }');
+    res.send('{ "response": "Hello From Faouzi Seddouki NodeJs" }');
 });
 
 app.get('/will', function (req, res) {
